53c6bce2ff6ba0ba63a00da639747c72b6c59d3c
Matías Quezada <matias.quezada@tcit.cl>
2021-12-02 21:01:27 -0300

    Initial commit

---
+++ src/main/java/com/tests/r1vs_allstreaming/R1vsAllStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.tests.r1vs_allstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R1vsAllStreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R1vsAllStreamingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tests/r1vs_allstreaming/R1vsAllStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tests.r1vs_allstreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R1vsAllStreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
